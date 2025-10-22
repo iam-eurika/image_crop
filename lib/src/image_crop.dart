f533eae240474173c89e053d16c7666978e9707b
EurikaOrmanel <wworgba@gmail.com>
2025-10-22 00:04:36 +0000

    removing drag  mixing and replacing hashValues() method

--- lib/src/image_crop.dart
+++ lib/src/image_crop.dart
@@ -4,13 +4,10 @@ class ImageOptions {
   final int width;
   final int height;
 
-  ImageOptions({
-    required this.width,
-    required this.height,
-  });
+  ImageOptions({required this.width, required this.height});
 
   @override
-  int get hashCode => hashValues(width, height);
+  int get hashCode => Object.hash(width, height);
 
   @override
   bool operator ==(other) =>
@@ -21,38 +18,36 @@ class ImageOptions {
 }
 
 class ImageCrop {
-  static const _channel =
-      const MethodChannel('plugins.lykhonis.com/image_crop');
+  static const _channel = const MethodChannel(
+    'plugins.lykhonis.com/image_crop',
+  );
 
   static Future<bool> requestPermissions() => _channel
       .invokeMethod('requestPermissions')
       .then<bool>((result) => result);
 
-  static Future<ImageOptions> getImageOptions({
-    required File file,
-  }) async {
-    final result =
-        await _channel.invokeMethod('getImageOptions', {'path': file.path});
+  static Future<ImageOptions> getImageOptions({required File file}) async {
+    final result = await _channel.invokeMethod('getImageOptions', {
+      'path': file.path,
+    });
 
-    return ImageOptions(
-      width: result['width'],
-      height: result['height'],
-    );
+    return ImageOptions(width: result['width'], height: result['height']);
   }
 
   static Future<File> cropImage({
     required File file,
     required Rect area,
     double? scale,
-  }) =>
-      _channel.invokeMethod('cropImage', {
+  }) => _channel
+      .invokeMethod('cropImage', {
         'path': file.path,
         'left': area.left,
         'top': area.top,
         'right': area.right,
         'bottom': area.bottom,
         'scale': scale ?? 1.0,
-      }).then<File>((result) => File(result));
+      })
+      .then<File>((result) => File(result));
 
   static Future<File> sampleImage({
     required File file,
@@ -64,7 +59,8 @@ class ImageCrop {
       if (preferredSize == null &&
           (preferredWidth == null || preferredHeight == null)) {
         throw ArgumentError(
-            'Preferred size or both width and height of a resampled image must be specified.');
+          'Preferred size or both width and height of a resampled image must be specified.',
+        );
       }
       return true;
     }());
